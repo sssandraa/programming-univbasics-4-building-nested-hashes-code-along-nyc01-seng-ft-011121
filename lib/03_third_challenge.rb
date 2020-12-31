100fa584f73094253c0410bfde6aaacc1caff511
Sandra Wazaz <sandrawazaz@gmail.com>
2020-12-31 21:17:13 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -14,7 +14,8 @@ def third_challenge
    },
    :capulet => {
       :patriarch => {
-        :name => "Lord Capulet", :age => "50"  },
+        :name => "Lord Capulet", :age => "50"
+        },
       :matriarch => {
       :name => "Lady Capulet", :age => "51"
         },
