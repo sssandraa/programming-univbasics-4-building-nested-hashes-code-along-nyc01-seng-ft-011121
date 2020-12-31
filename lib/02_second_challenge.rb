754cd388fada30f28641c1dbbf71ee6563d75892
Sandra Wazaz <sandrawazaz@gmail.com>
2020-12-31 21:08:22 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -7,7 +7,10 @@ def second_challenge
     :hero_friends => []
       },
     :capulet => {
-      #your key/value pairs here!
+    :patriarch => {},
+    :matriarch => {},
+    :hero => {},
+    :heroine_friends => []
       }}
 
 
